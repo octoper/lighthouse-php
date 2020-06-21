a2a549cc95f7ed2ea108b7d324aa791ac468a53f
Vaggelis Yfantis <vaggelisyfantis@gmail.com>
2020-06-21 15:34:26 +0000

    Apply fixes from StyleCI

--- tests/Unit/LighthouseTest.php
+++ tests/Unit/LighthouseTest.php
@@ -17,8 +17,8 @@ it('constructs the correct command', function () {
         'lighthouse',
         '--output=json',
         '--quiet',
-        "--config-path=/my/config",
-        "http://example.com",
+        '--config-path=/my/config',
+        'http://example.com',
         "--chrome-flags='--headless --disable-gpu --no-sandbox'",
     ]), $command);
 });
@@ -30,7 +30,6 @@ it('can_set_a_custom_node_binary', function () {
 
     $this->assertStringContainsString('/my/node/binary', $command);
 });
-
 
 it('can_set_chrome_flags', function () {
     $this->lighthouse->setChromeFlags('--my-flag');
@@ -47,7 +46,7 @@ it('can_set_the_output_file', function () {
 
     $command = $this->lighthouse->getCommand('http://example.com');
 
-    $this->assertStringContainsString("--output-path=/tmp/report.json", $command);
+    $this->assertStringContainsString('--output-path=/tmp/report.json', $command);
 });
 
 it('can_disable_device_emulation', function () {
@@ -77,41 +76,41 @@ it('can_disable_network_throttling', function () {
 it('can guess the output format from the file extension', function () {
     $this->lighthouse->setOutput('/tmp/report.json');
     $command = $this->lighthouse->getCommand('http://example.com');
-    $this->assertStringContainsString("--output=json", $command);
-    $this->assertStringNotContainsString("--output=html", $command);
+    $this->assertStringContainsString('--output=json', $command);
+    $this->assertStringNotContainsString('--output=html', $command);
 
     $this->lighthouse->setOutput('/tmp/report.html');
     $command = $this->lighthouse->getCommand('http://example.com');
-    $this->assertStringContainsString("--output=html", $command);
-    $this->assertStringNotContainsString("--output=json", $command);
+    $this->assertStringContainsString('--output=html', $command);
+    $this->assertStringNotContainsString('--output=json', $command);
 
     $this->lighthouse->setOutput('/tmp/report.md');
     $command = $this->lighthouse->getCommand('http://example.com');
-    $this->assertStringContainsString("--output=json", $command);
-    $this->assertStringNotContainsString("--output=html", $command);
+    $this->assertStringContainsString('--output=json', $command);
+    $this->assertStringNotContainsString('--output=html', $command);
 
     $this->lighthouse->setOutput('/tmp/report');
     $command = $this->lighthouse->getCommand('http://example.com');
-    $this->assertStringContainsString("--output=json", $command);
-    $this->assertStringNotContainsString("--output=html", $command);
+    $this->assertStringContainsString('--output=json', $command);
+    $this->assertStringNotContainsString('--output=html', $command);
 });
 
 it('can_override_the_output_format', function () {
     $this->lighthouse->setOutput('/tmp/report.json', 'html');
     $command = $this->lighthouse->getCommand('http://example.com');
-    $this->assertStringContainsString("--output=html", $command);
-    $this->assertStringNotContainsString("--output=json", $command);
+    $this->assertStringContainsString('--output=html', $command);
+    $this->assertStringNotContainsString('--output=json', $command);
 
     $this->lighthouse->setOutput('/tmp/report.md', ['html', 'json']);
     $command = $this->lighthouse->getCommand('http://example.com');
-    $this->assertStringContainsString("--output=html", $command);
-    $this->assertStringContainsString("--output=json", $command);
+    $this->assertStringContainsString('--output=html', $command);
+    $this->assertStringContainsString('--output=json', $command);
 
     $this->lighthouse->setOutput('/tmp/report.md', ['html', 'json', 'md']);
     $command = $this->lighthouse->getCommand('http://example.com');
-    $this->assertStringContainsString("--output=html", $command);
-    $this->assertStringContainsString("--output=json", $command);
-    $this->assertStringNotContainsString("--output=md", $command);
+    $this->assertStringContainsString('--output=html', $command);
+    $this->assertStringContainsString('--output=json', $command);
+    $this->assertStringNotContainsString('--output=md', $command);
 });
 
 it('cannot_add_the_same_category_multiple_times', function ($category, $method = null) {
@@ -138,7 +137,7 @@ it('can_set_the_headers_using_an_array', function () {
     $lighthouse = new MockLighthouse();
 
     $lighthouse->setHeaders([
-        'Cookie' => 'monster=blue',
+        'Cookie'        => 'monster=blue',
         'Authorization' => 'Bearer: ring',
     ]);
 
